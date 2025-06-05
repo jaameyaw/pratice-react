a8dfef76626e7bb7c873eadcb20a50aad3f42a9c
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-05 22:41:47 +0000

    config-container

--- Practice-react/src/App.jsx
+++ Practice-react/src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css'
 
 
+function QuestionOption ({children}) {
+  return (
+    <button className="question-btn">{children}</button>
+  )
+}
 
 function App() {
 
 
   return (
     <>
+      <div className="config-container">
+        <h2>How well do you know me?</h2>
 
+        <div className="config-option">
+          <h4>No of Questions</h4>
+          <div>
+
+          </div>
+
+        </div>
+        
+
+      </div>
     </>
   )
 }
