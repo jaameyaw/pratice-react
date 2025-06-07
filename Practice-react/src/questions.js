0970048fde1ad34f5456efa367d822f0bf4b65a3
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-07 02:07:41 +0000

    Create questions.js

---
+++ Practice-react/src/questions.js
@@ -0,0 +1,79 @@
+'strict mode';
+
+export const Questions = [
+  {
+    question: "What time of day am I most productive?",
+    options: ["Early Morning", "Afternoon", "Evening", "Midnight"],
+    answer: "Midnight"
+  },
+  {
+    question: "If I could only eat one thing forever, what would it be?",
+    options: ["Pizza", "Banku & tilapia", "Jollof rice", "Fried rice"],
+    answer: "Jollof rice"
+  },
+  {
+    question: "What’s my love language?",
+    options: ["Words of affirmation", "Quality time", "Acts of service", "Physical touch"],
+    answer: "Physical touch"
+  },
+  {
+    question: "What am I most likely doing on a Sunday afternoon?",
+    options: ["Sleeping", "Coding 👨🏽‍💻", "At church", "Watching a movie"],
+    answer: "At church"
+  },
+  {
+    question: "How would I react if you surprised me with food?",
+    options: ["Smile and say thanks", "Give you a big hug", "Scream with joy 😍", "Pretend I’m not excited"],
+    answer: "Give you a big hug"
+  },
+  {
+    question: "Which of these apps do I use the most?",
+    options: ["Instagram", "Snapchat", "YouTube", "WhatsApp"],
+    answer: "Snapchat"
+  },
+  {
+    question: "What’s my go-to way to relax?",
+    options: ["Listening to music", "Taking a nap", "Watching Netflix", "Coding something fun"],
+    answer: "Listening to music"
+  },
+  {
+    question: "When is my birthday?",
+    options: ["January 12", "March 15", "May 6", "August 20"],
+    answer: "May 6"
+  },
+  {
+    question: "What type of girls do I like?",
+    options: ["Dark-skinned ladies", "Slim ladies", "Tomboys", "Quiet girls"],
+    answer: "Slim ladies"
+  },
+  {
+    question: "What subject did I enjoy the most in school?",
+    options: ["Math", "English", "Biology", "History"],
+    answer: "Math"
+  },
+  {
+    question: "What’s one thing I never leave the house without?",
+    options: ["My phone", "My Bible", "My headphones", "My water bottle"],
+    answer: "My phone"
+  },
+  {
+    question: "What’s my biggest pet peeve?",
+    options: ["Loud chewing", "Slow walkers", "Being interrupted", "Messy desks"],
+    answer: "Being interrupted"
+  },
+  {
+    question: "What’s something I secretly enjoy?",
+    options: ["Dancing alone", "Watching cartoons", "Singing in the bathroom", "Playing video games"],
+    answer: "Singing in the bathroom"
+  },
+  {
+    question: "If I could travel anywhere right now, where would I go?",
+    options: ["Dubai", "USA", "Japan", "South Africa"],
+    answer: "USA"
+  },
+  {
+    question: "What would I probably do on a Friday night?",
+    options: ["Hang out with friends", "Stay home and code", "Watch movies", "Go for a walk"],
+    answer: "Stay home and code"
+  }
+]
